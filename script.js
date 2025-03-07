831073e1c1600bb0f19791fdfe39ab62f262bb38
ojoaobraga96 <vitor1996@hotmail.com>
2025-03-07 12:05:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificarSenha() {
+  var senha = document.getElementById("senha").value;
+  var senhaCorreta = "namorada123"; // Defina aqui a senha que você quer usar
+
+  if (senha === senhaCorreta) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("conteudo").style.display = "block";
+  } else {
+    alert("Senha incorreta! Tente novamente.");
+  }
+}
