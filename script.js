63bb592144a7fc7d7089843abf7279a2d6d9e861
ojoaobraga96 <vitor1996@hotmail.com>
2025-03-07 13:51:16 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function verificarSenha() {
   var senha = document.getElementById("senha").value;
-  var senhaCorreta = "namorada"; // Defina aqui a senha que você quer usar
+  var senhaCorreta = "nina"; // Defina aqui a senha que você quer usar
 
   if (senha === senhaCorreta) {
     document.getElementById("login").style.display = "none";
